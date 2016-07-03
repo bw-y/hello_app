c8a9b4fd61e657de41c8c2630a993c46d063bee4
bw.y <i@bw-y.com>
2016-07-03 22:49:43 +0800

    c3 finish

--- sample_app/app/helpers/application_helper.rb
+++ sample_app/app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # return full title require used page.   # show docs in display
+  def full_title(page_title = '')          # define function
+    base_title = "Ruby on Rails Tutorial Sample App"   # 变量赋值
+    if page_title.empty?                               # 布尔测试
+      base_title                                       # 隐式返回值
+    else
+      "#{page_title} | #{base_title}"                  # 字符串插值
+    end
+  end
 end
