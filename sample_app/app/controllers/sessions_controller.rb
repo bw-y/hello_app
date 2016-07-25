d96eb5a9c1efd40a644a2847539e4ae39e31c209
bw.y <i@bw-y.com>
2016-07-25 23:49:38 +0800

    next to 8.2.4

--- sample_app/app/controllers/sessions_controller.rb
+++ sample_app/app/controllers/sessions_controller.rb
@@ -6,6 +6,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       # login user, then rewrite user page
+      log_in user
+      redirect_to user
     else
       # create an error info
       # flash[:danger] = 'Invalid email/password combination' # 不完全正确
--- sample_app/app/helpers/sessions_helper.rb
+++ sample_app/app/helpers/sessions_helper.rb
@@ -1,2 +1,17 @@
 module SessionsHelper
+
+  # login a user
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  # return current user(if exist.)
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  # if user current, then return true, else false.
+  def logged_in?
+    @current_user.nil?
+  end
 end
